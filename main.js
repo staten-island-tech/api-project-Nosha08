60ab01bf7eaee77231bd80d99af3f0311dc4c700
Nosha08 <noah.rozin@outlook.com>
2023-12-13 12:41:26 -0500

    a

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const a  = 'nyc'
+DOMSelectors = {
+    form: document.querySelector('.form'),
+    container: document.querySelector('.container')
+}
+
+const a = DOMSelectors.form.value
 
 const URL = `https://api.tomorrow.io/v4/weather/realtime?location=${a}&apikey=iU0uqAKQxxH8mqAh1YJojL7oWqCDVPL1`
 async function getData(URL) {
@@ -6,6 +11,11 @@ async function getData(URL) {
         const response = await fetch(URL)
         const data = await response.json()
         console.log(data)
+        DOMSelectors.container.insertAdjacentHTML('afterbegin',`
+        <div class="card">
+            <h1>${data.location.name}</h1>
+            <h2>${data.data.temperature}</h2>
+        </div>`)
     } catch (error) {
         console.log(error)
     }
