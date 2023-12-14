0ddcdda5ccddecf55d581c8f8fbe6fd307fa4393
Nosha08 <noah.rozin@outlook.com>
2023-12-13 22:16:33 -0500

    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,9 +1,10 @@
 DOMSelectors = {
     form: document.querySelector('.form'),
+    location: document.querySelector('#location'),
     container: document.querySelector('.container')
 }
 
-const a = DOMSelectors.form.value
+const a = DOMSelectors.location.value
 
 const URL = `https://api.tomorrow.io/v4/weather/realtime?location=${a}&apikey=iU0uqAKQxxH8mqAh1YJojL7oWqCDVPL1`
 async function getData(URL) {
@@ -14,7 +15,7 @@ async function getData(URL) {
         DOMSelectors.container.insertAdjacentHTML('afterbegin',`
         <div class="card">
             <h1>${data.location.name}</h1>
-            <h2>${data.data.temperature}</h2>
+            <h2>${data.data.values.temperature}</h2>
         </div>`)
     } catch (error) {
         console.log(error)
