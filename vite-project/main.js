0a1e6f11e9f9ef01463c3e792c61666ae84f4f3b
Nosha08 <noah.rozin@outlook.com>
2023-12-14 10:57:36 -0500

    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,21 +4,26 @@ DOMSelectors = {
     container: document.querySelector('.container')
 }
 
-const a = DOMSelectors.location.value
+DOMSelectors.form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const a = DOMSelectors.location.value
 
-const URL = `https://api.tomorrow.io/v4/weather/realtime?location=${a}&apikey=iU0uqAKQxxH8mqAh1YJojL7oWqCDVPL1`
-async function getData(URL) {
-    try {
-        const response = await fetch(URL)
-        const data = await response.json()
-        console.log(data)
-        DOMSelectors.container.insertAdjacentHTML('afterbegin',`
-        <div class="card">
-            <h1>${data.location.name}</h1>
-            <h2>${data.data.values.temperature}</h2>
-        </div>`)
-    } catch (error) {
-        console.log(error)
+    const api = `https://api.tomorrow.io/v4/weather/realtime?location=${a}&apikey=iU0uqAKQxxH8mqAh1YJojL7oWqCDVPL1`
+    async function getData(api) {
+        try {
+            console.log(api)
+            const response = await fetch(api)
+            const data = await response.json()
+            console.log(data)
+            DOMSelectors.container.insertAdjacentHTML('beforeend',`
+            <div class="card">
+                <h1>${data.location.name}</h1>
+                <h2>${data.data.values.temperature}</h2>
+            </div>`)
+        } catch (error) {
+            console.log(error)
+        }
     }
-}
-getData(URL)
+    getData(api)
+})
+
