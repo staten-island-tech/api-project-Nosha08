f7c1987380419491719a5ebb83fe3c884d0c116c
Nosha08 <99495313+Nosha08@users.noreply.github.com>
2024-01-02 12:31:57 -0500

    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -61,26 +61,19 @@ function animate() {
 animate()
 
 if ("geolocation" in navigator) {
-    // Prompt user for permission to access their location
     navigator.geolocation.getCurrentPosition(
-      // Success callback function
       (position) => {
-        // Get the user's latitude and longitude coordinates
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
   
-        // Do something with the location data, e.g. display on a map
         console.log(`Latitude: ${lat}, longitude: ${lng}`);
         a(lat, lng)
       },
-      // Error callback function
       (error) => {
-        // Handle errors, e.g. user denied location sharing permissions
         console.error("Error getting user location:", error);
       }
     );
   } else {
-    // Geolocation is not supported by the browser
     console.error("Geolocation is not supported by this browser.");
   }
 
